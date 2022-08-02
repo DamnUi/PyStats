f84e789338f93637c197eca88a9ec1d44866b924
Noonuu <astrophysicsandpython@gmail.com>
2022-08-03 00:19:08 +0500

    [MAJOR-3]
    * added relative path option in __init__ method
    * added static methods about add_import_to_results and add_from_results
    * changed __scrape_import method
    * changed __scrape_variables method
    * added import_count method
    * added option of all or top N most_used_variables
    * separated imports in from_, import_, and all_ in get_import_names
    * added more functionalities to utilities.py
    * added test.py to .gitignore file

--- PyStats.py
+++ PyStats.py
@@ -1,21 +1,33 @@
 import argparse
-# Dashboard imports
+import ctypes
+import itertools
 import os
-from collections import Counter
+import re
+import sys
 
 from rich import pretty, print
-# Fine path called d:\1.Autohotkey\.Python\Project Folders\portswwww.py
 from rich.traceback import install as install_traceback
 
-# Usage: print(Panel.fit("Hello, [red]World!", title="Welcome", subtitle="Thank you"))
+from utilities import from_imports, import_imports, is_admin, list_to_counter_dictionary
+
 pretty.install()
 install_traceback(show_locals=False)
-# Dashboard imports \\ or pretty imports?
-
-# Initializing Parser
 
 if __name__ == '__main__':
+
+    os_name = os.name
+
+    if os_name == 'nt':
+        # we only want this to be executed if the operating system is Windows
+        # moved is_admin function to utilities.py
+        if is_admin():
+            print("Running the script with ADMIN privileges.")
+        else:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv),
+                                                None, 1)
+
     parser = argparse.ArgumentParser()
+    # if no arguments are passed, the default is set to the current directory
     parser.add_argument("-df", help="Input Absolute Path to Directory or File")
     # df is directory or file it will kinda figure out itself assuming theirs only 2 files in
     # the dir and u do -neglect
@@ -23,12 +35,30 @@ if __name__ == '__main__':
     parser.add_argument("-neglect", help="Input Absolute Path to File to Ignore ONLY IN DIRECTORY")
     # Debug argument to print out the file names
     path = parser.parse_args()
+    args = parser.parse_args()
     # 2 Variables
     # path.df
     # path.onefile ignores everything and makes sure u have just onefile ur scanning
     # Path.neglect
 
-    if path.onefile:
+    if not any(vars(args).values()):
+        # using different separators for windows and linux
+        separator = '\\' if os_name == 'nt' else '/'
+
+        # get file paths
+        paths = [f'{os.getcwd()}{separator}{py_file}' for py_file in os.listdir(os.getcwd()) if
+                 os.path.isfile(py_file) and py_file.endswith('.py')]
+
+        # get relative paths (full paths are too big)
+        paths = [os.path.relpath(path) for path in paths]
+
+        # sort
+        paths.sort()
+
+        # Automatic mode
+        print("Currently in Automatic mode this selects all files only in your current directory "
+              "ending with py extension")
+    elif path.onefile:
         paths = [path.onefile]
     else:
         # Determine if it's a directory or file
@@ -51,93 +81,161 @@ if __name__ == '__main__':
     # That ends classification of the file
 
 
+class OutputNotSpecified(Exception):
+    pass
+
+
 class Stat:
     def __init__(self, directory) -> None:
-        self.directory = directory
-        # Add .py to directory if not there
-        if not self.directory[-1].endswith('.py'):
-            self.directory[-1] += '.py'
+        self.directory = [os.path.relpath(x) for x in directory]
+        self.directory.sort()
         # Check if file exists
-        if not os.path.exists(self.directory[-1]):
-            print("[red]File does not exist")
+        if not all([x for x in self.directory]):
+            print("[red]No file present in given directory.")
             exit()
-        # The Neglect keyword ain't needed because it's already delt in the if statement from before
-
-    def scrape_imports(self):
-        imports_ = []
-        real_imports = []
-        if len(self.directory) > 1:
-            print("[green]Scraping imports from multiple files...")
-            for path in self.directory:
-                with open(path, encoding='utf8') as f:
-                    for line in f:
-                        if line.startswith('import'):
-                            imports_.append(line)
-                        elif line.startswith('from'):
-                            imports_.append(line)
         else:
-            print("[green]Scraping imports from single file...")
-            with open(self.directory[0], encoding='utf8') as f:
-                for line in f:
-                    if line.startswith('import'):
-                        imports_.append(line)
-                    elif line.startswith('from'):
-                        imports_.append(line)
+            dir_ = [x for x in self.directory if '__init__' in x]
+            print(f"[green]{len(self.directory)} files found in {len(dir_)} folders.\n")
+            # print("[yellow]Exploring further ...\n")
 
-        for line in imports_:
-            real_imports.append(line.strip())
+    @staticmethod
+    def add_imports_to_results(import_list, result_dictionary):
+        for key, value in import_list.items():
+            if key not in result_dictionary.keys():
+                result_dictionary[key] = value
+            else:
+                result_dictionary[key] += value
 
-        return real_imports
+    @staticmethod
+    def add_from_results(from_import_list, result_dictionary):
+        for key, value in from_import_list.items():
+            if key not in result_dictionary.keys():
+                result_dictionary[key] = value
+            else:
+                result_dictionary[key].extend(value)
 
-    def line_count(self):
-        # if it's a directory return the lines in a dict with the file name as the key
+    def __scrape_imports(self, get_assets=True):
+        result = {}
         if len(self.directory) > 1:
-            line_count = {}
-            for path in self.directory:
-                with open(path, encoding='utf8') as f:
-                    line_count[path] = len(f.readlines())
+            print("[blue]Scraping imports from multiple files...\n")
+            if get_assets:
+                print('[blue]Calculating assets ...\n')
+            for file_path in self.directory:
+                from_imp = from_imports(input_file=file_path, get_assets=get_assets)
+                imp_ = import_imports(input_file=file_path)
+
+                self.add_from_results(from_import_list=from_imp, result_dictionary=result)
+
+                self.add_imports_to_results(import_list=imp_, result_dictionary=result)
         else:
-            with open(self.directory[0], encoding='utf8') as f:
-                line_count = len(f.readlines())
+            print("[blue]Scraping imports from single file...\n")
+            from_imp = from_imports(input_file=self.directory[0], get_assets=get_assets)
+            imp_ = import_imports(input_file=self.directory[0])
+
+            self.add_from_results(from_import_list=from_imp, result_dictionary=result)
+
+            self.add_imports_to_results(import_list=imp_, result_dictionary=result)
+
+        return result
+
+    def __scrape_variables(self, full_line_of_variable=False):
+        variables_ = [line_
+                      for file_ in self.directory
+                      for line_ in open(file_, encoding='utf-8')
+                      if re.match('^\s*\w+\s=\s', line_) or re.match('^\s*self.\w+\s=\s', line_)]
+
+        list_of_variables = [variables.strip()
+                             if full_line_of_variable
+                             else variables.split('=')[0].strip().replace('self.', '')
+                             for variables in variables_
+                             if variables]
+
+        return list_of_variables
+
+    def import_count(self, get_assets=True):
+        result = self.__scrape_imports(get_assets=get_assets)
+
+        for key, value in result.items():
+            if not isinstance(value, int):
+                result[key] = list_to_counter_dictionary(value)
+
+        return dict(sorted(result.items(), reverse=True))
+
+    def line_count(self, exclude_empty_line: bool = False):
+        line_count = {}
+        # if it's a directory return the lines in a dict with the relative file name as the key
+        if len(self.directory) > 1:
+            for file_path in self.directory:
+                # changed the full file name to a relative path for easier viewing/showing
+                file_path = os.path.relpath(file_path)
+                with open(file_path, encoding='utf8') as open_file:
+
+                    # taken from https://stackoverflow.com/a/19001477
+                    # using generator expression, a LARGE file can also be read without
+                    # using too much physical memory of the system
+
+                    if not exclude_empty_line:
+                        count = sum(1 for _ in open_file)
+                    else:
+                        count = sum(1 for _ in open_file if _.rstrip('\n'))
+
+                    file_path = file_path.split('../')[1] if '..' in file_path else file_path
+                    line_count[file_path] = count
+        else:
+            with open(self.directory[0], encoding='utf8') as open_file:
+                file_path = os.path.relpath(self.directory[0])
+                line_count[file_path] = sum(1 for _ in open_file)
 
         return line_count
 
-    def most_used_variable(self):
-        variables = self.scrape_variables()
-        variable_count = Counter(variables)
-        dictionary = {}
-        for key, value in variable_count.items():
-            dictionary[key] = value
-        most_used_variable = max(dictionary, key=dictionary.get)
-        return most_used_variable, dictionary[most_used_variable]
+    def most_used_variable(self, n_variables=None):
+        if n_variables is None:
+            start = 'Frequency of variables used'
+        else:
+            start = f'Listing top {n_variables} variables used'
 
-    def scrape_variables(self, full_line_of_variable=False):
-        variables = []
-        var = {}
-        for i in self.directory:
-            with open(i, encoding='utf8') as f:
-                for line in f:
-                    if ' = ' in line:
-                        if full_line_of_variable:
-                            variables.append(line)
-                        variables.append(line.split()[0])
+        print(f'[magenta]{start}.\n')
 
-        return variables
+        most_used_variable = {key: value for key, value in
+                              sorted(list_to_counter_dictionary(self.__scrape_variables()).items(),
+                                     key=lambda item: item[1], reverse=True)}
 
-    def get_import_names(self):
-        imports = self.scrape_imports()
-        import_names = []
-        for line in imports:
-            if line.startswith('import'):
-                import_names.append(line.split()[1])
-            elif line.startswith('from'):
-                import_names.append(line.split()[3])
+        if n_variables is not None:
+            return {k: v for k, v in list(most_used_variable.items())[:n_variables]}
+        else:
+            return most_used_variable
 
-        return import_names
+    def get_import_names(self, import_type: str = 'all'):
+        all_imports = []
+        imports = self.import_count()
+
+        from_ = [[k_ for k_ in v.keys()] for k, v in imports.items() if 'from' in k]
+        from_ = list(set(itertools.chain.from_iterable(from_)))
+        from_.sort()
+
+        import_ = [k.split('import')[1].strip() for k in imports.keys() if 'import' in k]
+        import_.sort()
+
+        if import_type == 'from':
+            return from_
+        elif import_type == 'import':
+            return import_
+        elif import_type == 'all':
+            all_ = list(itertools.chain.from_iterable([from_, import_]))
+
+            all_imports.extend(all_)
+            all_imports = list(set(all_imports))
+            all_imports.sort()
+
+            return all_imports
+        else:
+            raise OutputNotSpecified('The out_import_type must be one of \'from\', \'import\', '
+                                     'or \'all\'.')
 
 
 info = Stat(paths)
-
-print(info.line_count(),
-      info.most_used_variable(),
-      info.scrape_variables())
+print(info.line_count())
+print(info.most_used_variable())
+# print(info.scrape_imports())
+#       info.most_used_variable(),
+#       info.scrape_variables())
---
+++ utilities.py
@@ -0,0 +1,66 @@
+"""Created on Jul 28 00:24:59 2022."""
+
+import ctypes
+import re
+from collections import Counter
+
+
+def AND(cond1, cond2):
+    return cond1 and cond2
+
+
+def OR(cond1, cond2):
+    return cond1 or cond2
+
+
+def list_to_counter_dictionary(list_):
+    return dict(Counter(list_))
+
+
+def from_imports(input_file, get_assets=False):
+    try:
+        result = []
+        with open(input_file, encoding='utf8') as input_file:
+            for _code_line in input_file:
+                if re.match("\bfrom\b\s\w+\simport\s[\w+,(?:\s\w+)?*]*", _code_line):
+                    # _code_line = _code_line.replace('from .', 'from ')
+                    result.append(_code_line)
+
+        if not get_assets:
+            result_ = [x.split('as')[0].strip() for x in result]
+        else:
+            assets = [_code_line.split('import')[1].split('as')[0].strip() for _code_line in result]
+            result = [_code_line.split('import')[0].strip() for _code_line in result]
+
+            result, assets = zip(*sorted(zip(result, assets)))
+
+            result_ = {key: [] for key in result}
+            for res_, ast_ in zip(result, assets):
+                result_[res_].extend([x.strip() for x in ast_.split(',')])
+
+        # change the key 'from' to 'from .' for the base directory imports
+        result_ = {'from .' if key == 'from' else key: value for key, value in result_.items()}
+        result = result_
+    except ValueError:
+        result = dict()
+
+    return result
+
+
+def import_imports(input_file):
+    import_ = []
+    with open(input_file, encoding='utf8') as input_file:
+        for _code_line in input_file:
+            _code_line = _code_line.strip()
+            if AND('import' in _code_line, 'from' not in _code_line):
+                _code_line = _code_line.split('as')[0]
+                import_.append(_code_line.strip())
+
+    return dict(Counter(import_))
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except AttributeError:
+        return False
