c1730cf1bfa232b60e365e8fe2e88c892cb30395
DamnUi <81849260+DamnUi@users.noreply.github.com>
2022-08-16 12:43:52 +0530

    Create func and class.py

---
+++ gex/func and class.py
@@ -0,0 +1,7 @@
+
+from rich import print
+from rich.markdown import Markdown
+
+
+print("[link=https://regex101.com/r/gv6WWf/2]Class[/link]")
+print("[link=https://regex101.com/r/NAtsiJ/1]Function[/link]")
