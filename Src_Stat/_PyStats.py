d14862b0a0894128ae567c564eef14127621bfd8
Nonja <anantakshg@gmail.com>
2022-09-13 19:33:35 +0530

    []

--- Src_Stat/_PyStats.py
+++ Src_Stat/_PyStats.py
@@ -811,3 +811,5 @@ class VisualWrapper:
                              title="[bright_black b]All Stats[/]",
                              title_align="center", style='red')
                 
+                
+                
